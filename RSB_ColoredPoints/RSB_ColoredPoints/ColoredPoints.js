b4e5c4b7d03fbefdd01e86484aabcb8249381203
ROBERT-PC\Robert <robertsmbj@gmail.com>
2018-09-16 17:59:57 -0500

    Adds ColoredPoints project and fixes MouseClick
    Adds ColoredPoints project and modifies MouseClick slightly for convenience.

--- RSB_ColoredPoints/RSB_ColoredPoints/ColoredPoints.js
+++ RSB_ColoredPoints/RSB_ColoredPoints/ColoredPoints.js
@@ -51,13 +51,10 @@ function main() {
     }
 
     //Register an event handler function that is called on mousepress
-    canvas.onmousedown = function(ev) { click(ev, gl, canvas, a_Position); };
+    canvas.onmousedown = function(ev) { click(ev, gl, canvas, a_Position, u_FragColor); };
 
-    //Pass position and point size to vertex shader's attribute values
-    gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+    //Pass point size to vertex shader's attribute values
     gl.vertexAttrib1f(a_PointSize, 10.0);
-    //Pass color to fragment shader's uniform value
-    gl.uniform4f(u_FragColor, 1.0, 0.0, 0.0, 1.0);
 
     //Set color for clearing canvas to black
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -67,7 +64,8 @@ function main() {
 }
 
 var g_points = []; //Array that holds mouse presses
-function click(ev, gl, canvas, a_Position) {
+var g_colors = []; //Array that holds the color of a point
+function click(ev, gl, canvas, a_Position, u_FragColor) {
     var x = ev.clientX; //X coordinate of mouse
     var y = ev.clientY; //Y coordinate of mouse
     var rect = ev.target.getBoundingClientRect();
@@ -77,16 +75,36 @@ function click(ev, gl, canvas, a_Position) {
     //Store the coordinates in the g_points array
     g_points.push([x, y]);
 
+    //Get the correct color based on click location
+    var color = getColor(x, y);
+    //Store the color in the g_colors array
+    g_colors.push(color);
+
     //Clear the canvas
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     var length = g_points.length;
     for (var i = 0; i < length; i++) {
         var xy = g_points[i];
+        var rgba = g_colors[i];
         //Pass the position of a point the the a_Position attribute variable 
         gl.vertexAttrib2f(a_Position, xy[0], xy[1]);
+        //Pass the color of a point to uFragColor variable
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
 
         //Draw a point
         gl.drawArrays(gl.POINTS, 0, 1);
+    }
+}
+
+function getColor(x, y) {
+    if (x >= 0.0 && y >= 0.0) { //First quadrant
+        return [1.0, 0.0, 0.0, 1.0]; //Red
+    }
+    else if (x < 0.0 && y < 0.0) { //Third quadrant
+        return [0.0, 1.0, 0.0, 1.0]; //Green
+    }
+    else { //Other quadrants
+        return [1.0, 1.0, 1.0, 1.0]; //White
     }
 }
