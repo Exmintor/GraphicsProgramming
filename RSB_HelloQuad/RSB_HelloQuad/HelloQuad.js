5577e0412c63cf4e82d4b5635909260d91f2c95d
ROBERT-PC\Robert <robertsmbj@gmail.com>
2018-09-17 14:46:35 -0500

    Adds the HelloQuad project

---
+++ RSB_HelloQuad/RSB_HelloQuad/HelloQuad.js
@@ -0,0 +1,82 @@
+﻿//Vertex Shader
+var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'void main() {\n' +
+    '   gl_Position = a_Position;\n' +
+    '}\n';
+
+//Fragment Shader
+var FSHADER_SOURCE =
+    'void main() {\n' +
+    '   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
+    '}\n';
+
+function main() {
+    //Retrieve canvas by ID
+    var canvas = document.getElementById('webgl');
+
+    //Get rendering context
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    //Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders.');
+        return;
+    }
+
+    //Set the position of vertices
+    var n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set the positions of the vertices');
+        return;
+    }
+
+    //Set color for clearing canvas to black
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    //Clear the canvas
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    //Draw two triangles (a rectangle)
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); //n is 3 in this context
+}
+
+function initVertexBuffers(gl) {
+    //An array of vertices
+    var vertices = new Float32Array([
+        -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5, -0.5  //TopLeft, BottomLeft, TopRight, BottomRight
+    ]);
+    var n = 4; //Number of vertices
+
+    //Create a buffer object
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create the buffer object.');
+        return -1;
+    }
+
+    //Bind the buffer object to the target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    //Write data into the buffer object
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    //Get storage location of attribute variable
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
+    }
+
+    //Assign the buffer object to a_Position variable
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+    //Enable the assignment to a_Position varibale
+    gl.enableVertexAttribArray(a_Position);
+
+    //Number of vertices in the buffer
+    return n;
+}
