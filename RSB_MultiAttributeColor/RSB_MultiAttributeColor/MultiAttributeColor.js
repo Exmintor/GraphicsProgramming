bdad9dfdf3f4170bc2ca688a13005f2a1dcf134d
ROBERT-PC\Robert <robertsmbj@gmail.com>
2018-09-24 20:01:45 -0500

    Adds the MultiAttributeColor project

---
+++ RSB_MultiAttributeColor/RSB_MultiAttributeColor/MultiAttributeColor.js
@@ -0,0 +1,102 @@
+﻿//Vertex Shader
+var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'attribute vec4 a_Color;\n' +
+    'varying vec4 v_Color;\n' +
+    'void main() {\n' +
+    '   gl_Position = a_Position;\n' +
+    '   gl_PointSize = 10.0;\n' +
+    '   v_Color = a_Color;\n' +
+    '}\n';
+
+//Fragment Shader
+var FSHADER_SOURCE =
+    'precision mediump float;\n' +
+    'varying vec4 v_Color;\n' +
+    'void main() {\n' +
+    '   gl_FragColor = v_Color;\n' +
+    '}\n';
+
+//Main Function
+function main() {
+    //Retrieve canvas by ID
+    var canvas = document.getElementById('webgl');
+
+    //Get rendering context
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    //Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders.');
+        return;
+    }
+
+    //Set the position of vertices
+    var n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set the positions of the vertices');
+        return;
+    }
+
+    //Set color for clearing canvas to black
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    //Clear the canvas
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    //Draw three points
+    gl.drawArrays(gl.POINTS, 0, n); //n is 3 in this context
+}
+
+function initVertexBuffers(gl) {
+    var verticesColors = new Float32Array([
+        0.0, 0.5, 1.0, 0.0, 0.0, //First vertex
+        -0.5, -0.5, 0.0, 1.0, 0.0, //Second vertex
+        0.5, -0.5, 0.0, 0.0, 1.0 //Third vertex
+    ]);
+    var n = 3; //Number of vertices
+
+    var FSIZE = verticesColors.BYTES_PER_ELEMENT;
+
+    //Create buffer object
+    var vertexColorBuffer = gl.createBuffer();
+    if (!vertexColorBuffer) {
+        console.log('Failed to create the buffer object.');
+        return -1;
+    }
+
+    //Bind the vertex buffer object the target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+    //Write data into the vertex buffer object
+    gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
+
+    //Get storage location of attribute variable
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
+    }
+
+    //Assign the buffer object to a_Position variable
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 5, 0);
+    //Enable the assignment to a_Position varibale
+    gl.enableVertexAttribArray(a_Position);
+
+
+    //Get storage location of attribute variable
+    var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+    if (a_Color < 0) {
+        console.log('Failed to get the storage location of a_Color');
+        return;
+    }
+    //Pass point size to vertex shader's attribute variable
+    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2);
+    //Enable the assignment to a_PointSize variable
+    gl.enableVertexAttribArray(a_Color);
+
+    return n;
+}
