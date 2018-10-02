05e4449d459cb3e9891195ef30981c32297c235c
ROBERT-PC\Robert <robertsmbj@gmail.com>
2018-10-01 21:19:33 -0500

    Adds the LookAtTriangles project

---
+++ RSB_LookAtTriangles/RSB_LookAtTriangles/LookAtTriangles.js
@@ -0,0 +1,126 @@
+﻿//Vertex Shader
+var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'attribute vec4 a_Color;\n' +
+    'uniform mat4 u_ViewMatrix;\n' +
+    'varying vec4 v_Color;\n' +
+    'void main() {\n' +
+    '   gl_Position = u_ViewMatrix * a_Position;\n' +
+    '   v_Color = a_Color;\n' +
+    '}\n';
+
+//Fragment Shader
+var FSHADER_SOURCE =
+    'precision mediump float;\n' +
+    'varying vec4 v_Color;\n' +
+    'void main() {\n' +
+    '   gl_FragColor = v_Color;\n' +
+    '}\n';
+
+//Main Function
+function main() {
+    //Retrieve canvas by ID
+    var canvas = document.getElementById('webgl');
+
+    //Get rendering context
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    //Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders.');
+        return;
+    }
+
+    //Set the position of vertices
+    var n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set the positions of the vertices');
+        return;
+    }
+
+    //Get storage location of the u_ViewMatrix variable
+    var u_ViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix');
+    if (u_ViewMatrix < 0) {
+        console.log('Failed to get the storage location of u_ViewMatrix');
+        return;
+    }
+
+    //Create the view matrix
+    var viewMatrix = new Matrix4();
+    // Set the eye point, look-at point, and the up direction
+    viewMatrix.setLookAt(0.20, 0.25, 0.25, 0, 0, 0, 0, 1, 0);
+
+    //Pass the view matrix to the vertex shader
+    gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements);
+
+    //Set color for clearing canvas to black
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    //Clear the canvas
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    //Draw three points
+    gl.drawArrays(gl.TRIANGLES, 0, n); //n is 3 in this context
+}
+
+function initVertexBuffers(gl) {
+    //Vertex coordinates and colors
+    var verticesColors = new Float32Array([
+        0.0, 0.5, -0.4, 0.4, 1.0, 0.4, // The back green triangle
+        -0.5, -0.5, -0.4, 0.4, 1.0, 0.4,
+        0.5, -0.5, -0.4, 1.0, 0.4, 0.4,
+
+        0.5, 0.4, -0.2, 1.0, 0.4, 0.4, // The middle yellow triangle
+        -0.5, 0.4, -0.2, 1.0, 1.0, 0.4,
+        0.0, -0.6, -0.2, 1.0, 1.0, 0.4,
+
+        0.0, 0.5, 0.0, 0.4, 0.4, 1.0, // The front blue triangle
+        -0.5, -0.5, 0.0, 0.4, 0.4, 1.0,
+        0.5, -0.5, 0.0, 1.0, 0.4, 0.4
+    ]);
+    var n = 9; //Number of vertices
+
+    var FSIZE = verticesColors.BYTES_PER_ELEMENT;
+
+    //Create buffer object
+    var vertexColorBuffer = gl.createBuffer();
+    if (!vertexColorBuffer) {
+        console.log('Failed to create the buffer object.');
+        return -1;
+    }
+
+    //Bind the vertex buffer object the target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+    //Write data into the vertex buffer object
+    gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
+
+    //Get storage location of attribute variable
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
+    }
+
+    //Assign the buffer object to a_Position variable
+    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, FSIZE * 6, 0);
+    //Enable the assignment to a_Position varibale
+    gl.enableVertexAttribArray(a_Position);
+
+
+    //Get storage location of attribute variable
+    var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+    if (a_Color < 0) {
+        console.log('Failed to get the storage location of a_Color');
+        return;
+    }
+    //Pass point size to vertex shader's attribute variable
+    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 6, FSIZE * 3);
+    //Enable the assignment to a_PointSize variable
+    gl.enableVertexAttribArray(a_Color);
+
+    return n;
+}
